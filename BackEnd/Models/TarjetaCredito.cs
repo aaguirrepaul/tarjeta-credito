2c5e878c6b69987768ce1726519a39e7b3786b1e
aaguirre <aaguirrepaul@gmail.com>
2020-12-19 12:52:52 -0300

    Primer commit

---
+++ BackEnd/Models/TarjetaCredito.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Models
+{
+    public class TarjetaCredito
+    {   [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public String Titular { get; set; }
+
+
+        [Required]
+        [Column(TypeName = "varchar(16)")]
+        public String NumeroTarjeta { get; set; }
+
+
+        [Required]
+        [Column(TypeName = "varchar(5)")]
+        public String FechaExpiracion { get; set; }
+
+
+        [Required]
+        [Column(TypeName = "varchar(3)")]
+        public String CVV { get; set; }
+
+
+
+
+
+
+    }
+}
